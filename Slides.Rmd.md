f590ef890ae53c81c659438c3f06b2291e0863d9
hemahecodes <helena.martin@alum.esci.upf.edu>
2020-06-07 19:01:06 +0200

    Study analysis

---
+++ Slides.Rmd.md
@@ -0,0 +1,19 @@
+Slides.Rmd
+========================================================
+author: Helena Martin Hernández
+date: 22/05/2020
+autosize: true
+
+First Slide
+========================================================
+
+Title: Amniotic fluid RNA gene expression profiling provides insights into the phenotype of Turner syndrome.
+
+Description: On this study, the fetal amniotic fluid is a complex biological material used to understand the Turner syndrome pathogenesis. Cell-free RNA from amniotic fluid of five mid-trimester Turner syndrome fetuses and five euploid female fetuses was extracted, amplified, and hybridized onto affymetrix arrays. Then, significantly differentially regulated genes were identified to understand which genes are responsible for the phenotypic differences on Turner's syndrome.
+========================================================
+
+Elements of th eexperimental design:
+- Response variable: Gene expression frenquency
+- Groups: 2 groups.
+- Sample size: 10 samples
+========================================================
---
+++ r.R
@@ -0,0 +1,106 @@
+# Version info: R 3.2.3, Biobase 2.30.0, GEOquery 2.40.0, limma 3.26.8
+# R scripts generated  Sun Jun 7 08:03:10 EDT 2020
+
+################################################################
+#   Differential expression analysis with limma
+library(Biobase)
+library(GEOquery)
+library(limma)
+
+# load series and platform data from GEO
+
+gset <- getGEO("GSE58435", GSEMatrix =TRUE, AnnotGPL=TRUE)
+if (length(gset) > 1) idx <- grep("GPL570", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+
+# make proper column names to match toptable 
+fvarLabels(gset) <- make.names(fvarLabels(gset))
+
+# group names for all samples
+gsms <- "1111100000"
+sml <- c()
+for (i in 1:nchar(gsms)) { sml[i] <- substr(gsms,i,i) }
+
+# log2 transform
+ex <- exprs(gset)
+qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
+LogC <- (qx[5] > 100) ||
+  (qx[6]-qx[1] > 50 && qx[2] > 0) ||
+  (qx[2] > 0 && qx[2] < 1 && qx[4] > 1 && qx[4] < 2)
+if (LogC) { ex[which(ex <= 0)] <- NaN
+exprs(gset) <- log2(ex) }
+
+# set up the data and proceed with analysis
+sml <- paste("G", sml, sep="")    # set group names
+fl <- as.factor(sml)
+gset$description <- fl
+design <- model.matrix(~ description + 0, gset)
+colnames(design) <- levels(fl)
+fit <- lmFit(gset, design)
+
+cont.matrix <- makeContrasts(G1-G0, levels=design)
+cont.matrix
+fit2 <- contrasts.fit(fit, cont.matrix)
+fit2 <- eBayes(fit2, 0.01)
+tT <- topTable(fit2, adjust="fdr", sort.by="B", number=250)
+
+tT <- subset(tT, select=c("ID","adj.P.Val","P.Value","t","B","logFC","Gene.symbol","Gene.title"))
+write.table(tT, file=stdout(), row.names=F, sep="\t")
+
+
+################################################################
+#   Boxplot for selected GEO samples
+library(Biobase)
+library(GEOquery)
+
+# load series and platform data from GEO
+
+gset <- getGEO("GSE58435", GSEMatrix =TRUE, getGPL=FALSE)
+if (length(gset) > 1) idx <- grep("GPL570", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+
+# group names for all samples in a series
+gsms <- "1111100000"
+sml <- c()
+for (i in 1:nchar(gsms)) { sml[i] <- substr(gsms,i,i) }
+sml <- paste("G", sml, sep="")  # set group names
+
+# order samples by group
+ex <- exprs(gset)[ , order(sml)]
+sml <- sml[order(sml)]
+fl <- as.factor(sml)
+labels <- c("Control","Turner")
+
+# set parameters and draw the plot
+palette(c("#f4dfdf","#dfeaf4", "#AABBCC"))
+dev.new(width=4+dim(gset)[[2]]/5, height=6)
+par(mar=c(2+round(max(nchar(sampleNames(gset)))/2),4,2,1))
+title <- paste ("GSE58435", '/', annotation(gset), " selected samples", sep ='')
+boxplot(ex, boxwex=0.6, notch=T, main=title, outline=FALSE, las=2, col=fl)
+legend("topleft", labels, fill=palette(), bty="n")
+
+
+
+
+
+
+library(mogene10sttranscriptcluster.db)
+all_anota<-data.frame(exprs(eset))
+Annot <- data.frame(SYMBOL=sapply(contents(mogene10sttranscriptclusterSYMBOL), paste, collapse=", "),
+                    DESC=sapply(contents(mogene10sttranscriptclusterGENENAME), paste, collapse=", "))
+Annot<-Annot[!Annot$SYMBOL=="NA",]
+Annot<-Annot[!Annot$DESC=="NA",]
+head(Annot)
+
+anotaGenes <- merge(Annot,all_anota, by.x=0,by.y=0)
+head(anotaGenes)
+write.table(anotaGenes,file="data.ann.txt",sep="\t")
+
+rownames(anotaGenes)<-anotaGenes[,1]
+anotaGenes<-anotaGenes[,-1]
+anotaGenes.end <- merge(anotaGenes,topTab, by.x=0,by.y=0)
+topTab.end<-anotaGenes.end[,c(1:3,12:17,4:11)]
+topTab.end<- topTab.end[order(-topTab.end$B),]
+
+rownames(topTab.end)<-topTab.end[,1]
+write.csv(topTab.end,file="TopTable.end.csv")
